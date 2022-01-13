c4aff163de9bec524203365dbfab1b16e07b21a7
TevenLeScao <teven.lescao@gmail.com>
2022-01-13 14:59:33 +0100

    new launch configs

--- scripts/gcp_launch_command.sh
+++ scripts/gcp_launch_command.sh
@@ -1,1 +1,18 @@
-accelerate launch tetraencoder/train.py  --model_name_or_path sentence-transformers/all-mpnet-base-v2 --kelm_file datasets/KELM/clean_kelm.jsonl --tekgen_file datasets/TEKGEN/processed-tekgen-train.jsonl --trex_file datasets/TREx/trex_graphs.jsonl --eval_sq_file datasets/SQ/wd/all_splits.csv --eval_webnlg_wikidata_file datasets/WebNLG_Wikidata/processed_webnlg_wikidata.jsonl --train_batch_size 40 --eval_batch_size 80 --output_dir outputs --num_epochs 1 --eval_steps 2000 --max_seq_length 384 --find_unused_parameters --hard_negatives --wandb --run_name all_datasets_bs320_hard_neg
+accelerate launch tetraencoder/train.py  \
+--model_name_or_path sentence-transformers/all-mpnet-base-v2 \
+--kelm_file datasets/KELM/clean_kelm.jsonl \
+--tekgen_file datasets/TEKGEN/processed-tekgen-train.jsonl \
+--trex_file datasets/TREx/trex_graphs.jsonl \
+--eval_sq_file datasets/SQ/wd/all_splits.csv \
+--eval_webnlg_wikidata_file datasets/WebNLG_Wikidata/processed_webnlg_wikidata.jsonl \
+--train_batch_size 24 \
+--eval_batch_size 80 \
+--output_dir outputs \
+--num_epochs 1 \
+--eval_steps 2000 \
+--checkpoint_save_steps 2000 \
+--max_seq_length 384 \
+--find_unused_parameters \
+--hard_negatives \
+--wandb \
+--run_name all_datasets_bs192_hard_neg
