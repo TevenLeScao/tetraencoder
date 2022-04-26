585c60739dda66a59086d58a6c4dfc42516b835c
TevenLeScao <teven.lescao@gmail.com>
2022-04-26 10:01:36 -0400

    eval_along_training.py improvements

--- tetraencoder/eval_along_training.py
+++ tetraencoder/eval_along_training.py
@@ -9,7 +9,7 @@ from accelerate import Accelerator
 from datasets import tqdm
 from sentence_transformers import SentenceTransformer
 
-from dataset_builders import *
+from dataset_wrappers import *
 from train import build_evaluators
 
 dataset_builders = {"msmarco": MsMarcoDataset, "kelm": KelmDataset, "gooaq": GooAqDataset, "tekgen": TekgenDataset,
@@ -38,15 +38,15 @@ def evaluate(model_path, sequential_evaluator, task_names, batch_size, wandb_log
     if wandb_log and is_main_process:
         for i, task_name in enumerate(task_names):
             if task_name == "MPWW":
-                wandb.log({f"{task_name}_accuracy@1": scores[i]["cos_sim"]["accuracy@k"][1],
+                wandb.log({f"MPWW_accuracy@1": scores[i]["accuracy@k"][1],
                            "data_points": seen_items})
-                wandb.log({f"{task_name}_recall@10": scores[i]["cos_sim"]["recall@k"][10],
+                wandb.log({f"MPWW_recall@10": scores[i]["recall@k"][10],
                            "data_points": seen_items})
                 wandb.log(
-                    {f"{task_name}_precision@10": scores[i]["cos_sim"]["precision@k"][10],
+                    {f"MPWW_precision@10": scores[i]["precision@k"][10],
                      "data_points": seen_items})
                 wandb.log(
-                    {f"{task_name}_MRR@10": scores[i]["cos_sim"]["mrr@k"][10], "data_points": seen_items})
+                    {f"MPWW_MRR@10": scores[i]["mrr@k"][10], "data_points": seen_items})
             else:
                 wandb.log({f"{task_name}_recall@1": scores[i]["recall@1_src2tgt"], "data_points": seen_items})
                 wandb.log(
@@ -62,31 +62,40 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     # model args
-    parser.add_argument("--checkpoints_folder", default=None, required=True, type=str)
     parser.add_argument("--max_seq_length", default=512, type=int)
     # i/o args
-    parser.add_argument("--output_dir", default=".", type=str)
+    parser.add_argument("--checkpoints_folder", default=None, required=True, type=str)
+    parser.add_argument("--only_last_checkpoint", action="store_true")
     # dataset args
     for dataset_name in dataset_builders:
         parser.add_argument(f"--{dataset_name}_file", default=None, type=str)
-    # evaluation dataset args
+    # various input sizes
+    parser.add_argument("--eval_corpus_chunk_size", default=16384, type=int)
     parser.add_argument("--eval_batch_size_per_gpu", default=64, type=int)
+    parser.add_argument("--faiss_index_training_samples", default=40*4096, type=int)
+    # evaluation dataset args
     parser.add_argument("--eval_webnlg_wikidata_file", default=None, type=str)
     parser.add_argument("--eval_webnlg_dbpedia_file", default=None, type=str)
     parser.add_argument("--eval_gooaq_file", default=None, type=str)
     parser.add_argument("--eval_sq_file", default=None, type=str)
     parser.add_argument("--eval_mpww_file", default=None, type=str)
     parser.add_argument("--eval_mpww_passages_file", default=None, type=str)
-    parser.add_argument("--eval_corpus_chunk_size", default=16384, type=int)
     # instrumentation
     parser.add_argument("--wandb", action="store_true")
     parser.add_argument("--run_name", default="test", type=str)
+    # use accelerate
+    parser.add_argument("--accelerate", action="store_true")
     # wrap-up
     args = parser.parse_args()
     print(args)
     assert os.path.isdir(args.checkpoints_folder)
 
-    accelerator = Accelerator()
+    if args.accelerate:
+        accelerator = Accelerator()
+        is_main_process = accelerator.is_main_process
+    else:
+        accelerator = None
+        is_main_process = True
     sequential_evaluator, task_names = build_evaluators(args)
 
     try:
@@ -99,7 +108,7 @@ if __name__ == "__main__":
         batch_size = 0
         neg_type = None
 
-    if args.wandb and accelerator.is_main_process:
+    if args.wandb and is_main_process:
         import wandb
         wandb.init(project="rdf-embeddings-evals", entity="flukeellington", name=args.run_name)
         wandb.config = {
@@ -109,6 +118,11 @@ if __name__ == "__main__":
             "max_seq_length": args.max_seq_length,
         }
 
-    for subfolder in tqdm(glob.glob(args.checkpoints_folder + "/[0-9]*")):
+    subfolders = glob.glob(args.checkpoints_folder + "/[0-9]*")
+    subfolders = sorted(subfolders, key=lambda folder: int(folder.split("/")[-1]))
+    if args.only_last_checkpoint:
+        subfolders = subfolders[-1:]
+
+    for subfolder in tqdm(subfolders):
         evaluate(subfolder, sequential_evaluator, task_names, batch_size, wandb_log=args.wandb,
-                 max_seq_length=args.max_seq_length, is_main_process=accelerator.is_main_process)
+                 max_seq_length=args.max_seq_length, is_main_process=is_main_process)
