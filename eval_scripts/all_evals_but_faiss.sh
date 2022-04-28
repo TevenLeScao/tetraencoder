fb29c85e3be168f8653c2c08a15db5b7cba3b9b5
TevenLeScao <teven.lescao@gmail.com>
2022-04-28 16:40:47 -0400

    added simple mpww to all_eval

--- eval_scripts/all_evals_but_faiss.sh
+++ eval_scripts/all_evals_but_faiss.sh
@@ -1,7 +1,7 @@
 accelerate launch tetraencoder/eval_along_training.py \
 --checkpoints_folder $1 \
---eval_batch_size_per_gpu 256 \
---eval_sq_file datasets/SQ/wd/all_splits.csv \
+--eval_batch_size_per_gpu 64 \
+--eval_simple_mpww_file /home/teven_huggingface_co/tetraencoder/datasets/MPWW/translation_style_mpww.jsonl \
 --eval_webnlg_wikidata_file datasets/WebNLG_Wikidata/processed_webnlg_wikidata.jsonl \
 --eval_webnlg_dbpedia_file datasets/WebNLG_DBpedia/processed_webnlg_dbpedia.jsonl \
 --max_seq_length 384 \
